af05effc9b7bab40cab86fded66cfa3ff91fb19d
YokoHermanto1 <yokohermanto@gmail.com>
2023-10-27 15:21:39 +0700

    adding file

---
+++ app/src/main/java/com/example/binus/tutorialpage.java
@@ -0,0 +1,14 @@
+package com.example.binus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tutorialpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorialpage);
+    }
+}
